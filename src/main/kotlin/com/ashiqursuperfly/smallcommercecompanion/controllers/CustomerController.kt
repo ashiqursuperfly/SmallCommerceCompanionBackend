b0ca0ce6cf792b81ab110cdfd4ca55c61236e912
ashiqursuperfly <ashiqur.buet16@gmail.com>
2022-01-18 03:59:43 +0600

    refactor

--- src/main/kotlin/com/ashiqursuperfly/smallcommercecompanion/controllers/CustomerController.kt
+++ src/main/kotlin/com/ashiqursuperfly/smallcommercecompanion/controllers/CustomerController.kt
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("api/v1/customer")
-class CustomerController: SimpleCrudController<Long, Customer, CustomerRepository>() {
+class CustomerController : SimpleCrudController<Long, Customer, CustomerRepository>() {
 
     @Autowired
     lateinit var sequenceGenerator: SequenceGeneratorService
@@ -29,34 +29,71 @@ class CustomerController: SimpleCrudController<Long, Customer, CustomerRepositor
         return customerRepository
     }
 
+    @GetMapping
+    fun get(@RequestHeader(required = true) secretAccessKey: String): ResponseEntity<ResponseModel<List<Customer>?>> {
+        val business = businessRepository.findBusinessBySecretAccessKey(secretAccessKey)
+            ?: return ResponseModel<List<Customer>?>(
+                data = null,
+                message = "Invalid/Missing business secret access key"
+            ).build(HttpStatus.FORBIDDEN)
+
+        val customers = customerRepository.findAllCustomersOfThisBusiness(business.id)
+
+        return ResponseModel<List<Customer>?>(data = customers).build(HttpStatus.OK)
+    }
+
     @GetMapping("/{id}")
-    fun get(@RequestHeader(required = true) secretAccessKey: String, @PathVariable id: Long): ResponseEntity<ResponseModel<Customer?>> {
+    fun get(
+        @RequestHeader(required = true) secretAccessKey: String,
+        @PathVariable id: Long
+    ): ResponseEntity<ResponseModel<Customer?>> {
         val customerResponse = super.get(id)
         val business = businessRepository.findBusinessBySecretAccessKey(secretAccessKey)
 
-        if(customerResponse.body?.data?.businessId != business?.id) {
-            return ResponseModel<Customer?>(data=null, message="This is not a customer of this business: ${business?.id}").build(HttpStatus.FORBIDDEN)
+        if (customerResponse.body?.data?.businessId != business?.id) {
+            return ResponseModel<Customer?>(
+                data = null,
+                message = "This is not a customer of this business: ${business?.id}"
+            ).build(HttpStatus.FORBIDDEN)
         }
         return customerResponse
     }
 
     @PostMapping
-    fun post(@RequestHeader(required = true) secretAccessKey: String, @RequestBody data: Customer): ResponseEntity<ResponseModel<Customer?>> {
-        val business = businessRepository.findBusinessBySecretAccessKey(secretAccessKey) ?: return ResponseModel<Customer?>(data=null, message="Invalid/Missing business secret access key").build(HttpStatus.FORBIDDEN)
+    fun post(
+        @RequestHeader(required = true) secretAccessKey: String,
+        @RequestBody data: Customer
+    ): ResponseEntity<ResponseModel<Customer?>> {
+        val business = businessRepository.findBusinessBySecretAccessKey(secretAccessKey)
+            ?: return ResponseModel<Customer?>(
+                data = null,
+                message = "Invalid/Missing business secret access key"
+            ).build(HttpStatus.FORBIDDEN)
 
         val copied = data.copy(
-            id=sequenceGenerator.generateSequence(Const.Mongo.SEQUENCES.CUSTOMER_SEQUENCE),
+            id = sequenceGenerator.generateSequence(Const.Mongo.SEQUENCES.CUSTOMER_SEQUENCE),
             businessId = business.id
         )
         return super.post(copied)
     }
 
     @PutMapping("/{id}")
-    fun put(@RequestHeader(required = true) secretAccessKey: String, @RequestBody data: Customer, @PathVariable id: Long): ResponseEntity<ResponseModel<Customer?>> {
-        val business = businessRepository.findBusinessBySecretAccessKey(secretAccessKey) ?: return ResponseModel<Customer?>(data=null, message="Invalid/Missing business secret access key").build(HttpStatus.FORBIDDEN)
+    fun put(
+        @RequestHeader(required = true) secretAccessKey: String,
+        @RequestBody data: Customer,
+        @PathVariable id: Long
+    ): ResponseEntity<ResponseModel<Customer?>> {
+        val business = businessRepository.findBusinessBySecretAccessKey(secretAccessKey)
+            ?: return ResponseModel<Customer?>(
+                data = null,
+                message = "Invalid/Missing business secret access key"
+            ).build(HttpStatus.FORBIDDEN)
         val customer = super.get(id)
-        if(customer.body?.data?.businessId != business.id) {
-            return ResponseModel<Customer?>(data=null, message="This is not a customer of this business: ${business.id}").build(HttpStatus.FORBIDDEN)
+        if (customer.body?.data?.businessId != business.id) {
+            return ResponseModel<Customer?>(
+                data = null,
+                message = "This is not a customer of this business: ${business.id}"
+            ).build(HttpStatus.FORBIDDEN)
         }
         return super.put(id, data)
     }
