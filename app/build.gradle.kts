1282b51a52852e5e81f402d3c6ed32f16a73f26f
ashiqursuperfly <ashiqur.buet16@gmail.com>
2022-01-21 10:16:12 +0600

    force https on heroku

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,11 +21,13 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-
+    //swagger
     implementation("io.springfox:springfox-boot-starter:3.0.0")
     implementation("io.springfox:springfox-swagger2:3.0.0")
     implementation("io.springfox:springfox-swagger-ui:3.0.0")
-
+    //security
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.security:spring-security-test")
 
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
---
+++ app/src/main/kotlin/com/ashiqursuperfly/smallcommercecompanion/configurations/WebSecurityConfig.kt
@@ -0,0 +1,21 @@
+package com.ashiqursuperfly.smallcommercecompanion.configurations
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.web.util.matcher.RequestMatcher
+import javax.servlet.http.HttpServletRequest
+
+@Configuration
+class WebSecurityConfig : WebSecurityConfigurerAdapter() {
+  @Throws(Exception::class)
+  override fun configure(http: HttpSecurity) {
+    http.requiresChannel()
+      .requestMatchers(RequestMatcher { r: HttpServletRequest ->
+        r.getHeader(
+          "X-Forwarded-Proto"
+        ) != null
+      })
+      .requiresSecure()
+  }
+}
