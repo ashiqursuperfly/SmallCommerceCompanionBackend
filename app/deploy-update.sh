644fa068ada2fae4e2c1ab41f8bff79d6f000c61
ashiqursuperfly <ashiqur.buet16@gmail.com>
2022-02-12 04:31:10 +0600

    refactor

--- app/deploy-update.sh
+++ app/deploy-update.sh
@@ -1,5 +1,5 @@
 ./gradlew clean
 ./gradlew bootJar
-cp build/libs/*.jar ../heroku/heroku-remote/app.jar
-cd ../heroku/heroku-remote
+cp build/libs/*.jar /home/ashiq/Documents/Workspaces/Spring-Workspace/SmallCommerceCompanionHeroku/heroku-remote/scom2b/app.jar
+cd /home/ashiq/Documents/Workspaces/Spring-Workspace/SmallCommerceCompanionHeroku/heroku-remote/scom2b
 bash deploy.sh
